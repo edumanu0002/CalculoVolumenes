11aba5003bf2e041c15a61cfa7aed19e2a0eb761
edumanu0002 <60758207+edumanu0002@users.noreply.github.com>
2025-02-05 22:59:53 +0100

    Add files via upload

---
+++ Prog1_0.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Prog1_0 {
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+		Scanner leerradio = new Scanner(System.in);
+		int radio;
+		System.out.println("Introduce el radio de la esfera");
+		radio = leerradio.nextInt();
+	}
+}
---
+++ Prog1_2_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Prog1_2_1 {
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+		Scanner leerradio = new Scanner(System.in);
+		int radio;
+		System.out.println("Introduce el radio de la esfera");
+		radio = leerradio.nextInt();
+		double volumen;
+		volumen = (4 * (Math.PI) * (Math.pow(radio, 3)) / 3);
+		System.out.println("El volumen de la esfera cuyo radio " + radio + " es " + volumen);
+		System.out.println("Finalizado Version 1");
+
+	}
+}
